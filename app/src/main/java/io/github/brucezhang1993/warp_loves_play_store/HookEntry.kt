cc6f7fc6e05d72180737fd5a894c9278b09f43ed
Bruce Zhang <zttt183525594@gmail.com>
2024-09-30 11:56:40 +0800

    use base hooker

--- app/src/main/java/io/github/brucezhang1993/warp_loves_play_store/HookEntry.kt
+++ app/src/main/java/io/github/brucezhang1993/warp_loves_play_store/HookEntry.kt
@@ -15,21 +15,6 @@ object HookEntry : IYukiHookXposedInit {
     }
 
     override fun onHook() = YukiHookAPI.encase {
-        loadApp("com.cloudflare.onedotonedotonedotone") {
-            "android.net.VpnService\$Builder".toClass().method {
-                name = "addDisallowedApplication"
-                param(StringClass)
-                returnType = "android.net.VpnService\$Builder".toClass()
-            }.hook {
-                before {
-                    val param1 = args().first().string();
-                    if ("com.android.vending" == param1) {
-                        result = instanceOrNull
-                        return@before
-                    }
-                    result = callOriginal()
-                }
-            }
-        }
+        loadApp("com.cloudflare.onedotonedotonedotone", PlayStoreWhitelistHooker)
     }
 }
---
+++ app/src/main/java/io/github/brucezhang1993/warp_loves_play_store/PlayStoreWhitelistHooker.kt
@@ -0,0 +1,24 @@
+package io.github.brucezhang1993.warp_loves_play_store
+
+import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
+import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.java.StringClass
+
+object PlayStoreWhitelistHooker : YukiBaseHooker() {
+    override fun onHook() {
+        "android.net.VpnService\$Builder".toClass().method {
+            name = "addDisallowedApplication"
+            param(StringClass)
+            returnType = "android.net.VpnService\$Builder".toClass()
+        }.hook {
+            before {
+                val param1 = args().first().string();
+                if ("com.android.vending" == param1) {
+                    result = instanceOrNull
+                    return@before
+                }
+                result = callOriginal()
+            }
+        }
+    }
+}
